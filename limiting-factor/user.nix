8253e537a830ba0368140339e7ce9f9fdf06008f
Torgny Bjers <torgny@bjers.org>
2023-04-30 23:34:22 -0400

    fleek: apply

--- limiting-factor/user.nix
+++ limiting-factor/user.nix
@@ -1,13 +1,13 @@
-{ pkgs, misc, lib, ... }: {
+{ pkgs, misc, lib, config, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek.
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
 
   systemd.user.tmpfiles.rules = [
-    "L /home/tbjers/Desktop - - - - /home/tbjers/sync/Desktop"
-    "L /home/tbjers/Documents - - - - /home/tbjers/sync/Documents"
-    "L /home/tbjers/Music - - - - /home/tbjers/sync/Music"
-    "L /home/tbjers/Pictures - - - - /home/tbjers/sync/Pictures"
-    "L /home/tbjers/Templates - - - - /home/tbjers/sync/Templates"
-    "L /home/tbjers/Videos - - - - /home/tbjers/sync/Videos"
+    "L ${config.home.homeDirectory}/Desktop - - - - ${config.home.homeDirectory}/sync/Desktop"
+    "L ${config.home.homeDirectory}/Documents - - - - ${config.home.homeDirectory}/sync/Documents"
+    "L ${config.home.homeDirectory}/Music - - - - ${config.home.homeDirectory}/sync/Music"
+    "L ${config.home.homeDirectory}/Pictures - - - - ${config.home.homeDirectory}/sync/Pictures"
+    "L ${config.home.homeDirectory}/Templates - - - - ${config.home.homeDirectory}/sync/Templates"
+    "L ${config.home.homeDirectory}/Videos - - - - ${config.home.homeDirectory}/sync/Videos"
   ];
 }
